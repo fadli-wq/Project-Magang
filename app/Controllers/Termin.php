4115dbb943b9977845ba56945db6cf23d0cc43d3
fadli-wq <dayfos123.com>
2025-04-17 13:49:23 +0700

    memperbaiki jumlah termin di halaman termin

--- app/Controllers/Termin.php
+++ app/Controllers/Termin.php
@@ -81,90 +81,42 @@ class Termin extends Controller
     $pembayaranTModel = new \App\Models\TenderPembayaranModel();
     $pembayaranPModel = new \App\Models\PlPembayaranModel();
 
-    // Ambil semua termin
     $termins = $terminModel->findAll();
-
-    // Ambil semua kontrak dari e-katalog dan tender
     $ekatalog = $ekatalogModel->findAll();
     $tender = $tenderModel->findAll();
     $pl = $plModel->findAll();
 
-    $pembayaranE = $pembayaranEModel->findAll();
-    $pembayaranT = $pembayaranTModel->findAll();
-
-    // Gabungkan semua kontrak ke dalam satu array dengan id & nama
-    $kontrak = [];
     $kontrakList = [];
     $jumlahTerminPerKontrak = [];
-    // foreach ($ekatalog as $k) {
-    //     $kontrak[$k['id']] = '[E-Katalog] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
-    // }
 
-    // foreach ($tender as $t) {
-    //     $kontrak[$t['id']] = '[Tender] ' . ($t['nomor_kontrak'] ?? '-') . ' - ' . $t['nama'];
-    // }
-    // foreach ($pl as $p) {
-    //   $kontrak[$p['id']] = '[Tender] ' . ($p['nomor_kontrak'] ?? '-') . ' - ' . $p['nama'];
-    // }
-
+    // Gabungkan semua kontrak ke dalam array
     foreach ($ekatalog as $k) {
-      $kontrakList['e' . $k['id']] = '[E-Katalog] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
+        $id = 'e' . $k['id'];
+        $kontrakList[$id] = '[E-Katalog] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
+
+        $pembayaran = $pembayaranEModel->where('id_kontrak', $k['id'])->first();
+        $jumlahTerminPerKontrak[$id] = $pembayaran['jumlah_termin'] ?? '-';
     }
-    
-    // Kontrak Tender
+
     foreach ($tender as $k) {
-        $kontrakList['t' . $k['id']] = '[Tender] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
+        $id = 't' . $k['id'];
+        $kontrakList[$id] = '[Tender] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
+
+        $pembayaran = $pembayaranTModel->where('id_kontrak', $k['id'])->first();
+        $jumlahTerminPerKontrak[$id] = $pembayaran['jumlah_termin'] ?? '-';
     }
-    
-    // Kontrak PL
+
     foreach ($pl as $k) {
-        $kontrakList['p' . $k['id']] = '[PL] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
+        $id = 'p' . $k['id'];
+        $kontrakList[$id] = '[PL] ' . ($k['nomor_kontrak'] ?? '-') . ' - ' . $k['nama'];
+
+        $pembayaran = $pembayaranPModel->where('id_kontrak', $k['id'])->first();
+        $jumlahTerminPerKontrak[$id] = $pembayaran['jumlah_termin'] ?? '-';
     }
 
-    foreach ($ekatalog as &$k) {
-      $pembayaran = $pembayaranEModel->where('id_kontrak',$k['id'])->first();
-      $k['jumlah_termin'] = $pembayaran['jumlah_termin'] ?? '-';
-    }
-
-    foreach ($tender as &$t) {
-        $pembayaran = $pembayaranTModel->where('id_kontrak',$t['id'])->first();
-        $t['jumlah_termin'] = $pembayaran['jumlah_termin'] ?? '-';
-    }
-    foreach ($pl as &$p) {
-        $pembayaran = $pembayaranPModel->where('id_kontrak',$p['id'])->first();
-        $p['jumlah_termin'] = $pembayaran['jumlah_termin'] ?? '-';
-    }
-
-    // Hitung jumlah termin untuk setiap kontrak
-    foreach ($termins as $t) {
-        $id = $t['kontrak_id']; // Sudah dalam bentuk 'e5', 't1', dst
-        if (!isset($jumlahTerminPerKontrak[$id])) {
-            $jumlahTerminPerKontrak[$id] = 0;
-        }
-        $jumlahTerminPerKontrak[$id]++;
-    }
-
-
-  //   foreach ($termins as &$termin) {
-  //     $id = $termin['kontrak_id'];
-  
-  //     if (isset($kontrakList['e' . $id])) {
-  //         $termin['kontrak_id_prefixed'] = 'e' . $id;
-  //     } elseif (isset($kontrakList['t' . $id])) {
-  //         $termin['kontrak_id_prefixed'] = 't' . $id;
-  //     } elseif (isset($kontrakList['p' . $id])) {
-  //         $termin['kontrak_id_prefixed'] = 'p' . $id;
-  //     } else {
-  //         $termin['kontrak_id_prefixed'] = null;
-  //     }
-  // }
-  
     return view('termin/daftar termin', [
         'termins' => $termins,
         'kontrakList' => $kontrakList,
-        'ekatalog' => $ekatalog,
-        'pl' => $pl,
-        'tender' => $tender,
         'jumlahTerminPerKontrak' => $jumlahTerminPerKontrak
     ]);
     }
